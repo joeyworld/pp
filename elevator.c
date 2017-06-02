fe478d6be19777c8fae0f5d2212200ec388c24f5
DESKTOP-LN8IKJQ\Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-06-02 17:18:25 +0900

    UI 화면 및 요청 받기 구현

--- elevator.c
+++ elevator.c
@@ -36,6 +36,7 @@ typedef struct _QUEUE {
 typedef struct _ELEVATOR {
     int floor[FLOOR]; //각 층에 멈추는지 여부
     int is_moving; //운행 중인지 여부
+    int current_floor; //현재 위치해 있는 층
     int curr_people; //현재 타고있는 사람 수
     int total_people; //현재까지 총 태운 사람 수
     int fix; //수리 중인지 여부
@@ -46,6 +47,8 @@ typedef struct _ELEVATOR {
 typedef struct _INPUT {
     char *menu_input;
     int *floor_input;
+    int *dest_input;
+    int *people_input;
 }Input;
 
 /* simul_t thread에서 필요한 변수 */
@@ -60,12 +63,15 @@ void *input_f(void *data); //input_t thread에서 실행되는 함수
 void *simul_f(void *data); //simul_t thread에서 실행되는 함수
 void allocate(Input **input, Simul **simul); //동적할당 처리
 void init(Elevator *elevators[6]);
+void print_UI(Elevator *elevators[6]);
 void print_elevator_info(Elevator *elevators[6]);
-void print_menu();
+void print_menu(char mode);
 char get_menu_input();
+void get_request(Input *input);
 void quit(Elevator *elevators[6]);
 void simul_stop(char *mode);
 void simul_restart(int *time, Simul *simul);
+Elevator *call_elevator(Elevator *elevators[6]); //움직여야 할 엘리베이터 찾는 함수
 
 int main(void) {
     Input *input;
@@ -91,6 +97,8 @@ int main(void) {
     pthread_join(simul_t, NULL);
 
     free(simul);
+    free(input->people_input);
+    free(input->dest_input);
     free(input->floor_input);
     free(input->menu_input);
     free(input);
@@ -102,27 +110,38 @@ void allocate(Input **input, Simul **simul) {
     *input = (Input *)malloc(sizeof(Input));
     (*input)->menu_input = (char *)malloc(sizeof(char));
     (*input)->floor_input = (int *)malloc(sizeof(int));
+    (*input)->dest_input = (int *)malloc(sizeof(int));
+    (*input)->people_input = (int *)malloc(sizeof(int));
     *simul = (Simul *)malloc(sizeof(Simul));
     (*simul)->input = *input;
 }
 
 void *input_f(void *data) {
     Input *input = (Input *)data;
-    while(*input->menu_input != 'Q') {
-        *input->menu_input = get_menu_input();
+    while(1) {
+        if(*input->menu_input == QUIT) {
+            break;
+        } else if(*input->menu_input == CALL) {
+            get_request(input);
+        } else {
+            *input->menu_input = get_menu_input();
+        }
     }
 }
 
 void *simul_f(void *data) {
     int time = 0;
+    Elevator *to_move = NULL;
     Simul *simul = (Simul *)data;
     init(simul->elevators);
     while(1) {
+        //gotoxy(0, 0);
         system("clear");
-        //print_UI();
+        print_UI(simul->elevators);
         printf("시뮬레이션 시작한 지 %d 초 경과 \n", time);
+        printf("현재 요청 : %d층->%d층 %d명 \n", *simul->input->floor_input, *simul->input->dest_input, *simul->input->people_input);
         print_elevator_info(simul->elevators);
-        print_menu();
+        print_menu(*simul->input->menu_input);
 
         if(*simul->input->menu_input == QUIT) {
             quit(simul->elevators);
@@ -131,7 +150,11 @@ void *simul_f(void *data) {
         } else if(*simul->input->menu_input == RESTART) {
             simul_restart(&time, simul);
             continue;
-        } else if(*simul->input->menu_input == CALL) {
+        }
+
+        //Elevator call
+        to_move = call_elevator(simul->elevators);
+        if(to_move != NULL) {
         }
 
         time++;
@@ -165,11 +188,34 @@ void init(Elevator *elevators[6]) {
         }
 
         //다른 변수들 초기값 설정
+        elevators[i]->current_floor = 1;
         elevators[i]->is_moving = 0;
         elevators[i]->curr_people = 0;
         elevators[i]->total_people = 0;
         elevators[i]->fix = 0;
     }
+}
+
+void print_UI(Elevator *elevators[6]) {
+    int i, j;
+    for(i = 0; i < FLOOR; i++) {
+        printf("    ------------------------------------------------------ \n"); //윗칸
+        printf("%2dF ", FLOOR - i);
+        for(j = 0; j < NUM_ELEVATORS; j++) {
+            if(elevators[j]->current_floor == FLOOR - i) {
+                printf("|");
+                if(elevators[j]->is_moving == 0) {
+                    printf(" 대기중 ");
+                } else {
+                    //TODO further implementation when moving
+                }
+            } else {
+                printf("|        ");
+            }
+        }
+        printf("| \n");
+    }
+    printf("    ------------------------------------------------------ \n"); //아랫칸
 }
 
 void print_elevator_info(Elevator *elevators[6]) {
@@ -187,15 +233,20 @@ void print_elevator_info(Elevator *elevators[6]) {
     }
 }
 
-void print_menu() {
-    printf("-----------MENU------------ \n");
-    printf("Q : 종료       \t");
-    printf("W : 정지       \n");
-    printf("E : 재개       \t");
-    printf("R : 재시작     \n");
-    printf("A : 호출       \n");
-    printf("--------------------------- \n");
-    printf("메뉴 선택 : ");
+void print_menu(char mode) {
+    if(mode == CALL) {
+        printf("---엘리베이터 호출 모드--- \n");
+        printf("현재 층, 목적 층, 사람 수 순서대로 : ");
+    } else {
+        printf("-----------MENU------------ \n");
+        printf("Q : 종료       \t");
+        printf("W : 정지       \n");
+        printf("E : 재개       \t");
+        printf("R : 재시작     \n");
+        printf("A : 호출       \n");
+        printf("--------------------------- \n");
+        printf("메뉴 선택 : ");
+    }
     fflush(stdout);
 }
 
@@ -203,6 +254,11 @@ char get_menu_input() {
     char input;
     scanf(" %c", &input);
     return input;
+}
+
+void get_request(Input *input) {
+    scanf(" %d %d %d", input->floor_input, input->dest_input, input->people_input);
+    *input->menu_input = 0;
 }
 
 void quit(Elevator *elevators[6]) {
@@ -224,8 +280,7 @@ void quit(Elevator *elevators[6]) {
 
 //TODO implementation of stop, resume, and restart
 void simul_stop(char *mode) {
-    while(*mode != RESUME && *mode != QUIT) {
-        sleep(1);
+    while(*mode != RESUME && *mode != QUIT && *mode != RESTART) {
     }
 }
 
